efc89dcf3cc257d4dd23e3d44e8f928e05f5f1a3
Jin Hyuk Park <llvslr77@gmail.com>
2020-01-26 05:14:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   sen_arr = sentence.split(" ")
   new_arr = []
-  new_arr << sen_arr.each{|word| word.reverse}
+  new_arr << sen_arr.map{|word| word.reverse}
   new_arr.join(" ")
 end
